9c8c6e0a51eb957f05966363b9d65bb3acbf17b1
unknown <pawat.kriti@bumail.net>
2026-08-24 11:23:26 +0700

    Part A

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace Lab02
+﻿/*
+ * Student ID : 167070353
+ * Name       : Phawat krittiyaphinit
+ * Section    : 129A
+ * No.        : 
+ * Course     : GI113 Computer Programming (GI)
+ */
+
+namespace Lab02
 {
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string bossName = "Kirin";
+            char rank = 'S';
+            int level = 7;
+            int maxHp = 240;
+            int currentHp = 175;
+            float attackPower = 42.5f;
+            double critMultiplier = 1.75;
+            bool isBoss = true;
+
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            Console.WriteLine($"Name: {bossName}");
+            Console.WriteLine($"Rank: {rank}");
+            Console.WriteLine($"Level: {level}");
+            Console.WriteLine($"HP: {currentHp}/{maxHp}");
+            Console.WriteLine();
+
+            int hpPercent = currentHp * 100 / maxHp;
+            Console.WriteLine($"Hp Percent: {hpPercent}%");
+            Console.WriteLine();
+            Console.WriteLine("Kirin takes 60 damage!");
+            Console.WriteLine();
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            Console.WriteLine($"Hp: {currentHp} / {maxHp}");
+            Console.WriteLine($"Hp Percent: 47%");
         }
     }
 }
