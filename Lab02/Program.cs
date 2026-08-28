a7f51e9e29c4ee733365887a08aac1af58de50ed
pawatkriti-hash <pawat.kriti@bumail.net>
2026-08-28 15:04:31 +0700

    Lab02

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -13,14 +13,29 @@ namespace Lab02
     {
         static void Main(string[] args)
         {
-            string bossName = "Kirin";
+            Program1();
+            Console.WriteLine();
+            Program2();
+            Console.WriteLine();
+            Program3();
+            Console.WriteLine();
+            Program4();
+            Console.WriteLine();
+            Program5();
+            Console.WriteLine();
+        }
+        static void Program1()
+        {
+            string bossName = "Prem,The Void Emperor";
             char rank = 'S';
-            int level = 7;
-            int maxHp = 240;
-            int currentHp = 175;
-            float attackPower = 42.5f;
+            int level = 10000000;
+            int maxHp = 240000000;
+            int currentHp = 97500;
+            float attackPower = 978000.5f;
             double critMultiplier = 1.75;
             bool isBoss = true;
+
+
 
             Console.WriteLine("===== BOSS STATUS: INITIAL =====");
             Console.WriteLine($"Name: {bossName}");
@@ -39,7 +54,124 @@ namespace Lab02
             Console.WriteLine($"Hp Percent: 47%");
 
         }
-        
-    }
-    
-}
+
+        static void Program2()
+        {
+            string bossName = "Super Gay Mon";
+            char rank = 'E';
+            int level = 100;
+            int maxHp = 400;
+            int currentHp = 235;
+            float attackPower = 72.5f;
+            double critMultiplier = 5.75;
+            bool isBoss = true;
+
+
+
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            Console.WriteLine($"Name: {bossName}");
+            Console.WriteLine($"Rank: {rank}");
+            Console.WriteLine($"Level: {level}");
+            Console.WriteLine($"HP: {currentHp}/{maxHp}");
+            Console.WriteLine();
+
+            int hpPercent = currentHp * 100 / maxHp;
+            Console.WriteLine($"Hp Percent: {hpPercent}%");
+            Console.WriteLine();
+            Console.WriteLine("Kirin takes 60 damage!");
+            Console.WriteLine();
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            Console.WriteLine($"Hp: {currentHp} / {maxHp}");
+            Console.WriteLine($"Hp Percent: 47%");
+
+        }
+        static void Program3()
+        {
+            string bossName = "Brother Square";
+            char rank = 'F';
+            int level = 1000;
+            int maxHp = 1;
+            int currentHp = 2;
+            float attackPower = 10000000.5f;
+            double critMultiplier = 0.15;
+            bool isBoss = true;
+
+
+
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            Console.WriteLine($"Name: {bossName}");
+            Console.WriteLine($"Rank: {rank}");
+            Console.WriteLine($"Level: {level}");
+            Console.WriteLine($"HP: {currentHp}/{maxHp}");
+            Console.WriteLine();
+
+            int hpPercent = currentHp * 100 / maxHp;
+            Console.WriteLine($"Hp Percent: {hpPercent}%");
+            Console.WriteLine();
+            Console.WriteLine("Kirin takes 60 damage!");
+            Console.WriteLine();
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            Console.WriteLine($"Hp: {currentHp} / {maxHp}");
+            Console.WriteLine($"Hp Percent: 47%");
+
+        }
+        static void Program4()
+        {
+            string bossName = "AoiOverlord";
+            char rank = 'A';
+            int level = 700;
+            int maxHp = 240000;
+            int currentHp = 17000;
+            float attackPower = 42000.5f;
+            double critMultiplier = 1.75;
+            bool isBoss = true;
+
+
+
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            Console.WriteLine($"Name: {bossName}");
+            Console.WriteLine($"Rank: {rank}");
+            Console.WriteLine($"Level: {level}");
+            Console.WriteLine($"HP: {currentHp}/{maxHp}");
+            Console.WriteLine();
+
+            int hpPercent = currentHp * 100 / maxHp;
+            Console.WriteLine($"Hp Percent: {hpPercent}%");
+            Console.WriteLine();
+            Console.WriteLine("Kirin takes 60 damage!");
+            Console.WriteLine();
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            Console.WriteLine($"Hp: {currentHp} / {maxHp}");
+            Console.WriteLine($"Hp Percent: 47%");
+
+        }
+        static void Program5()
+        {
+            string bossName = "God Nine";
+            char rank = 'G';
+            int level = 1;
+            int maxHp = 999999999;
+            int currentHp = 999999999;
+            float attackPower = 999999999.5f;
+            double critMultiplier = 999999999.75;
+            bool isBoss = true;
+
+
+
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            Console.WriteLine($"Name: {bossName}");
+            Console.WriteLine($"Rank: {rank}");
+            Console.WriteLine($"Level: {level}");
+            Console.WriteLine($"HP: {currentHp}/{maxHp}");
+            Console.WriteLine();
+
+            int hpPercent = currentHp * 100 / maxHp;
+            Console.WriteLine($"Hp Percent: {hpPercent}%");
+            Console.WriteLine();
+            Console.WriteLine("Kirin takes 60 damage!");
+            Console.WriteLine();
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            Console.WriteLine($"Hp: {currentHp} / {maxHp}");
+            Console.WriteLine($"Hp Percent: 47%");
+
+        }
