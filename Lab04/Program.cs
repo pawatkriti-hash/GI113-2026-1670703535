88a12be039af38161e055d1f0b8f22cbf2823cf7
pawatkriti-hash <pawat.kriti@bumail.net>
2026-09-11 11:38:10 +0700

    Lab04

---
+++ Lab04/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Lab04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("+_+_+ NEW ADVENTURE +_+_+"); // Print ธรรมดา Title
+            //Console.Write("Name your hero: "); //WriteLine พิมพ์บรรทัดใหม่, Write พิมพ์ต่อบรรทัดเดิม
+
+            //string playerName = Console.ReadLine(); // เมื่อผู้เล่นกรอกชื่อ จากนั้นชื่อจะเข้าไปเก็บใน playerName
+
+            //Console.WriteLine($"\nWelcome, {playerName}. Your journey begins...");
+
+            //Console.WriteLine("\n+++++++++++++++++++++" +
+            //"\n+ Difficulty Selecion +" +
+            //"\n+++++++++++++++++++++");
+            //Console.Write("Choose Difficulty (1-3): ");
+            //string choice = Console.ReadLine(); // รับ Input และเก็บใส่ choice
+
+            //int difficulty = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"Difficulty set to {difficulty}");
+
+            //Console.WriteLine("\\\\\\ ITEM SHOP //////");
+            //Console.Write("How many potions?: ");
+            //bool isValid = int.TryParse(Console.ReadLine(), out int quantity);
+            //Console.WriteLine($">> Valid input: {isValid}");
+            //Console.WriteLine($">> Quantity: {quantity} potions");
+
+            Console.WriteLine("Welcome to the Adventure Game!");
+            Console.WriteLine("+----------------------------+" +
+                               "\n|      Character Creation    |" +
+                               "\n+----------------------------+");
+            Console.Write("Name your character: ");
+            string charName = Console.ReadLine();
+            Console.Write("Choose a class (1-3): ");
+            bool classValid = int.TryParse(Console.ReadLine(), out int classNum);
+            Console.Write("Staring luck (0.0-10.0): ");
+            bool luckValid = double.TryParse(Console.ReadLine(), out double luckNum);
+            Console.WriteLine($"{charName} the Class-{classNum} adventurer enters the dungeon. Luck: {luckNum}");
+        }
+    }
+}
